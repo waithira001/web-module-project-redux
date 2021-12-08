55ab7f01ed33552fe3ec577a5e12ee537985e0d5
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-08 00:28:23 -0600

    Update FavoriteMovieList.js

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { deleteFavorites } from '../actions/favoritesActions';
+
 
 const FavoriteMovieList = (props) => {
     const favorites = [];
